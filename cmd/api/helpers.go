837bc0b4acbc989305a8110ae3096766bbc041b4
Nikita <obrikash@gmail.com>
2025-02-16 19:32:13 +0300

    validator package for validating input JSON, created establishing a connection pool

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -74,7 +74,7 @@ func (app *application) readJSON(w http.ResponseWriter, r *http.Request, dst any
 			return errors.New("body must not be empty")
 
 		case strings.HasPrefix(err.Error(), "json: unknown field "):
-			fieldName := strings.TrimPrefix(err.Error(), "json: unknown feild ")
+			fieldName := strings.TrimPrefix(err.Error(), "json: unknown field ")
 			return fmt.Errorf("body contains unknown key %s", fieldName)
 
 		case errors.As(err, &maxBytesError):
