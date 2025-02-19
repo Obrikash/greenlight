5e9cb4f62334c5c535b2ecc43f09be2ea01fcc8d
Nikita <obrikash@gmail.com>
2025-02-19 23:31:17 +0300

    validation for page, page_size and sort values, also /v1/movies lists all movies from database

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -169,9 +169,7 @@ func (app *application) listMoviesHandler(w http.ResponseWriter, r *http.Request
 	var input struct {
 		Title    string
 		Genres   []string
-		Page     int
-		PageSize int
-		Sort     string
+        data.Filters
 	}
 
 	v := validator.New()
@@ -185,11 +183,22 @@ func (app *application) listMoviesHandler(w http.ResponseWriter, r *http.Request
 	input.PageSize = app.readInt(qs, "page_size", 20, v)
 
 	input.Sort = app.readString(qs, "sort", "id")
+    input.SortSafeList = []string{"id", "title", "year", "runtime", "-id", "-title", "-year", "-runtime"}
+    
+    if data.ValidateFilters(v, input.Filters); !v.Valid() {
+        app.failedValidationResponse(w, r, v.Errors)
+        return
+    }
 
-	if !v.Valid() {
-		app.failedValidationResponse(w, r, v.Errors)
-	}
-
-    fmt.Fprintf(w, "%+v\n", input)
+    movies, err := app.models.Movies.GetAll(input.Title, input.Genres, input.Filters)
+    if err != nil {
+        app.serverErrorResponse(w, r, err)
+        return
+    }
+    
+    err = app.writeJSON(w, http.StatusOK, envelope{"movies": movies}, nil)
+    if err != nil {
+        app.serverErrorResponse(w, r, err)
+    }
 
 }
