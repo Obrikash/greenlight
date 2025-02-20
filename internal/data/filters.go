6f66512a3b9052f0850a1cfac3077047911fc392
Nikita <obrikash@gmail.com>
2025-02-20 14:00:25 +0300

    query sort parameter by column (i.e. sort=-runtime), migration for index gin for full-text search for movies' titles

--- internal/data/filters.go
+++ internal/data/filters.go
@@ -1,6 +1,10 @@
 package data
 
-import "github.com/obrikash/greenlight/internal/validator"
+import (
+	"strings"
+
+	"github.com/obrikash/greenlight/internal/validator"
+)
 
 type Filters struct {
 	Page     int
@@ -18,4 +22,20 @@ func ValidateFilters(v *validator.Validator, f Filters) {
     v.Check(validator.PermittedValue(f.Sort, f.SortSafeList...), "sort", "invalid sort value")
 }
 
+func (f Filters) sortColumn() string {
+    for _, safeValue := range f.SortSafeList {
+        if f.Sort == safeValue {
+            return strings.TrimPrefix(f.Sort, "-")
+        }
+    }
+
+    panic("unsafe sort parameter: " + f.Sort)
+}
+
+func (f Filters) sortDirection() string {
+    if strings.HasPrefix(f.Sort, "-") {
+        return "DESC"
+    }
+    return "ASC"
+}
 
