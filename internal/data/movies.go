223af2f235327ae7b81052f024c81b05a5e32d03
Nikita <obrikash@gmail.com>
2025-02-20 15:16:33 +0300

    pagination and corresponding json response for getting metadata pagination /v1/movies

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -138,24 +138,26 @@ func (m MovieModel) Delete(id int64) error {
 	return nil
 }
 
-func (m MovieModel) GetAll(title string, genres []string, filters Filters) ([]*Movie, error) {
-    query := fmt.Sprintf(`SELECT id, created_at, title, year, runtime, genres, version
+func (m MovieModel) GetAll(title string, genres []string, filters Filters) ([]*Movie, Metadata, error) {
+    query := fmt.Sprintf(`SELECT count(*) OVER(), id, created_at, title, year, runtime, genres, version
         FROM movies
         WHERE (to_tsvector('simple', title) @@ plainto_tsquery('simple', $1) OR $1 = '')
         AND (genres @> $2 OR $2 = '{}')
-        ORDER BY %s %s, id ASC`, filters.sortColumn(), filters.sortDirection())
+        ORDER BY %s %s, id ASC
+        LIMIT $3 OFFSET $4`, filters.sortColumn(), filters.sortDirection())
 
 
     ctx, cancel := context.WithTimeout(context.Background(), 3 * time.Second)
     defer cancel()
 
-    rows, err := m.DB.QueryContext(ctx, query, title, pq.Array(genres))
+    rows, err := m.DB.QueryContext(ctx, query, title, pq.Array(genres), filters.limit(), filters.offset())
     if err != nil {
-        return nil, err
+        return nil, Metadata{}, err
     }
 
     defer rows.Close()
 
+    totalRecords := 0
     movies := []*Movie{}
 
     for rows.Next() {
@@ -163,6 +165,7 @@ func (m MovieModel) GetAll(title string, genres []string, filters Filters) ([]*M
 
 
         err := rows.Scan(
+            &totalRecords,
             &movie.ID,
             &movie.CreatedAt,
             &movie.Title,
@@ -172,15 +175,17 @@ func (m MovieModel) GetAll(title string, genres []string, filters Filters) ([]*M
             &movie.Version,
         )
         if err != nil {
-            return nil, err
+            return nil, Metadata{}, err
         }
 
         movies = append(movies, &movie)
     }
 
     if err = rows.Err(); err != nil {
-        return nil, err
+        return nil, Metadata{}, err
     }
 
-    return movies, nil
+    metadata := calculateMetadata(totalRecords, filters.Page, filters.PageSize)
+
+    return movies, metadata ,nil
 }
