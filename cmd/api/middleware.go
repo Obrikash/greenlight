2eb6a0ec3b7be28dcc3f2f08f5cf8a572ab02a4d
Nikita <obrikash@gmail.com>
2025-02-21 15:48:41 +0300

    graceful shutdown, User model (insert, get, update) with sql migrations

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -52,7 +52,9 @@ func (app *application) rateLimit(next http.Handler) http.Handler {
         }
     }()
 
+
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+        if app.config.limiter.enabled {
 		ip, _, err := net.SplitHostPort(r.RemoteAddr)
 		if err != nil {
 			app.serverErrorResponse(w, r, err)
@@ -62,7 +64,7 @@ func (app *application) rateLimit(next http.Handler) http.Handler {
 		mu.Lock()
 
 		if _, found := clients[ip]; !found {
-            clients[ip] = &client{limiter: rate.NewLimiter(2, 4)}
+            clients[ip] = &client{limiter: rate.NewLimiter(rate.Limit(app.config.limiter.rps), app.config.limiter.burst)}
 		}
 
         clients[ip].lastSeen = time.Now()
@@ -74,6 +76,7 @@ func (app *application) rateLimit(next http.Handler) http.Handler {
 		}
 
         mu.Unlock()
+    }
 
 		next.ServeHTTP(w, r)
 	})
